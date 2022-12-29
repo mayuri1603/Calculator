041936af505d9fcada6dd94ed4e3d1ccb026922f
mayuri1603 <mrparaskar@gmail.com>
2022-12-28 14:49:25 -1200

    Cal

---
+++ src/main/java/com/coder/Calculater.java
@@ -0,0 +1,24 @@
+package com.coder;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Calculater
+ */
+@WebServlet("/Calculater")
+public class Calculater extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Calculater() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
